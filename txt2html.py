05f0e81f57a5ddc0faf0b6fabdbcf3eb54fda389
hswmartin <hswmartin@gmail.com>
2016-11-04 21:32:16 +0800

    error throw

--- txt2html.py
+++ txt2html.py
@@ -3,15 +3,20 @@ import os,sys,re
 with open("1.html","w") as of:
     of.write("<meta http-equiv=\"content-type\" content=\"text/html; charset=gbk\">\n")
 with open(sys.argv[1],"r") as f:
-    for t in f.readlines()[2:]:
-        t=t.replace(' ',"&nbsp;").decode("utf-8").encode("gbk")
+    for t in f.readlines():
+        try:
+            t=t.replace(' ',"&nbsp;").decode("utf-8").encode("gbk")
+        except UnicodeEncodeError as e:
+            t=""
         if re.search('@@.*@@',t)<>None:
             t=t.replace(re.search('@@.*@@',t).group(),"")
         if t.startswith("+"):
             t="<p style=\"line-height:50%\"><font color=green>"+t.rstrip()+"</font></p>\n"
         elif t.startswith("-"):
             t = "<p style=\"line-height:50%\"><font color=red><s>" + t.rstrip() + "</s></font></p>\n"
-        else:
+        elif t.startswith("&nbsp;"):
             t = "<p style=\"line-height:50%\">"+t.rstrip()+"</p>\n"
+        else:
+            t =""
         with open(os.path.splitext(sys.argv[1])[0]+".html","a+") as of:
             of.write(t)
